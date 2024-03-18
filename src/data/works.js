cf3d014522dd73c7720e5af7d83d293336a8120e
abid-1294 <abidulislam316@gmail.com>
2024-03-19 03:41:27 +0600

    image moved in assets file

--- src/data/works.js
+++ src/data/works.js
@@ -2,28 +2,28 @@ export default [
 	{
 		title: 'Agenc IT Farm',
 		imgUrl:
-			'/src/images/Agenc-it-farm.png',
+			'../assets/Agenc-it-farm.png',
 		tech: ['HTML', 'CSS', 'Bootstrap'],
 		workUrl: 'https://agenc-it-farm.vercel.app',
 	},
 	{
 		title: "Real State Webpage",
 		imgUrl:
-			'/src/images/Real State.png',
+			'../assets/Real State.png',
 		tech: ['React JS', 'Tailwind CSS'],
 		workUrl: 'https://real-state-page-working.vercel.app/',
 	},
 	{
 		title: "Explore Nature",
 		imgUrl:
-			'/src/images/Explore Nature.png',
+			'../assets/Explore Nature.png',
 		tech: ['HTML', 'CSS'],
 		workUrl: 'https://explore-nature-demo.vercel.app/',
 	},
 	{
 		title: 'Innovate IT Farm',
 		imgUrl:
-			'/src/images/Innovate.png',
+			'../assets/Innovate.png',
 		tech: ['HTML', 'CSS'],
 		workUrl: 'https://innovate-it-farm-webpage.vercel.app/',
 	},
