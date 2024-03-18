ec271c14da3ca47428161506b7f46fc86b92b0c6
abid-1294 <abidulislam316@gmail.com>
2024-03-19 03:47:35 +0600

    image bug fixed

--- src/data/works.js
+++ src/data/works.js
@@ -1,29 +1,30 @@
+import AgencImg from '../assets/Agenc-it-farm.png';
+import RealEstateImg from '../assets/Real State.png';
+import NatureImg from '../assets/Explore Nature.png';
+import InnovateImg from '../assets/Innovate.png';
+
 export default [
 	{
 		title: 'Agenc IT Farm',
-		imgUrl:
-			'../assets/Agenc-it-farm.png',
+		imgUrl: {AgencImg},
 		tech: ['HTML', 'CSS', 'Bootstrap'],
 		workUrl: 'https://agenc-it-farm.vercel.app',
 	},
 	{
 		title: "Real State Webpage",
-		imgUrl:
-			'../assets/Real State.png',
+		imgUrl: {RealEstateImg},
 		tech: ['React JS', 'Tailwind CSS'],
 		workUrl: 'https://real-state-page-working.vercel.app/',
 	},
 	{
 		title: "Explore Nature",
-		imgUrl:
-			'../assets/Explore Nature.png',
+		imgUrl: {NatureImg},
 		tech: ['HTML', 'CSS'],
 		workUrl: 'https://explore-nature-demo.vercel.app/',
 	},
 	{
 		title: 'Innovate IT Farm',
-		imgUrl:
-			'../assets/Innovate.png',
+		imgUrl: {InnovateImg},
 		tech: ['HTML', 'CSS'],
 		workUrl: 'https://innovate-it-farm-webpage.vercel.app/',
 	},
